c66fd6b92c3c77029129f2fb0f04c807b658b1aa
Ashwiniganesun <ashwini.ganesun@freshworks.com>
2021-05-27 13:38:07 +0530

    Finish user edit, update, index, and destroy actions

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,9 +25,9 @@ class User < ActiveRecord::Base
   validates :password, presence: true, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
-  private
+private
 
     def create_remember_token
-      self.remember_token = SecureRandom.urlsafe_base64
+      self.remember_token = SecureRandom.hex
     end
 end
